e31fb7c10f99e09adb22f17a08409f1da4d1a424
Valentin <galikvalkin@gmail.com>
2019-06-22 15:47:40 +0300

    removed init function due to issue with events overlapping

--- index.js
+++ index.js
@@ -8,27 +8,9 @@ import * as React from "react";
 import KV from "./keyboardAvoidingView";
 import SV from "./scrollView";
 
-const init = (
-  EVENT_TYPES: string[],
-) => {
-  return {
-    KeyboardAvoidingView: React.forwardRef((props, ref) => (
-      <KV
-        {...props}
-        ref={ref}
-        customEvents={EVENT_TYPES}
-      />
-    )),
-    ScrollView: React.forwardRef((props, ref) => (
-      <SV
-        {...props}
-        ref={ref}
-        customEvents={EVENT_TYPES}
-      />
-    )),
-  };
-}
-
 export * from "./helpers";
 
-export default init;
+export default {
+  KeyboardAvoidingView: KV,
+  ScrollView: SV,
+};
